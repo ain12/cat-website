9f0933293be6c22404f46b8c827d2d0253c1a490
Ainhoa <ainhoaactic@gmail.com>
2021-03-23 16:16:03 +0100

    modal window with shadow

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector(".menu");
 const toggleHeader = document.querySelector(".toggle-menu");
+const overlay = document.querySelector(".overlay");
 menu.addEventListener("click", function () {
-  if (toggleHeader.style.visibility == "visible") {
+  if (
+    overlay.style.visibility == "visible" &&
+    toggleHeader.style.visibility == "visible"
+  ) {
+    overlay.style.visibility = "hidden";
     toggleHeader.style.visibility = "hidden";
   } else {
+    overlay.style.visibility = "visible";
     toggleHeader.style.visibility = "visible";
   }
 });
